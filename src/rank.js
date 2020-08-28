338913c80a55752297306855aa2dff7aa1000d43
Ellie04162606 <1181140808@qq.com>
2020-08-28 21:45:34 +0800

    refactor: extract length  more than rank

--- src/rank.js
+++ src/rank.js
@@ -16,28 +16,8 @@ function isLengthLessThan5(length) {
   return length < 5;
 }
 
-function isLengthMoreThan4(length) {
-  return length > 4;
-}
-
-function isLengthMoreThan8(length) {
-  return length > 8;
-}
-
-function isLengthMoreThan10(length) {
-  return length > 10;
-}
-
-function isLengthMoreThan12(length) {
-  return length > 12;
-}
-
-function isLengthMoreThan14(length) {
-  return length > 14;
-}
-
-function isLengthMoreThan18(length) {
-  return length > 18;
+function isLengthMoreThanRank(rank,length) {
+  return length > rank;
 }
 
 function checkRating(vpf, vr, chr) {
@@ -46,8 +26,8 @@ function checkRating(vpf, vr, chr) {
 
 function voyageRisk(voyage) {
   let result = 1;
-  isLengthMoreThan4(voyage.length) ? (result += 2) : result;
-  isLengthMoreThan8(voyage.length) ? (result += voyage.length - 8) : result;
+  isLengthMoreThanRank(4,voyage.length) ? (result += 2) : result;
+  isLengthMoreThanRank(8,voyage.length) ? (result += voyage.length - 8) : result;
   isIncludeChinaAndIndies(voyage.zone) ? (result += 4) : result;
   return Math.max(result, 0);
 }
@@ -56,7 +36,6 @@ function captainHistoryRisk(voyage, history) {
   let result = 1;
   isLengthLessThan5(history.length) ? (result += 4) : result;
   result += history.filter((v) => v.profit < 0).length;
-
   if (isIncludeChina(voyage.zone) && hasChina(history)) {
     result -= 2;
   }
@@ -68,16 +47,15 @@ function voyageProfitFactor(voyage, history) {
   isIncludeChinaAndIndies(voyage.zone) ? (result += 1) : result;
   if (isIncludeChina(voyage.zone) && hasChina(history)) {
     result += 3;
-    isLengthMoreThan10(history.length) ? (result += 1) : result;
-    isLengthMoreThan12(voyage.length) ? (result += 1) : result;
-    isLengthMoreThan18(voyage.length) ? (result -= 1) : result;
+    isLengthMoreThanRank(10,history.length) ? (result += 1) : result;
+    isLengthMoreThanRank(12,voyage.length) ? (result += 1) : result;
+    isLengthMoreThanRank(18,voyage.length) ? (result -= 1) : result;
   } else {
-    isLengthMoreThan8(history.length) ? (result += 1) : result;
-    isLengthMoreThan14(voyage.length) ? (result -= 1) : result;
+    isLengthMoreThanRank(8,history.length) ? (result += 1) : result;
+    isLengthMoreThanRank(14,voyage.length) ? (result -= 1) : result;
   }
   return result;
 }
-
 
 function rating(voyage, history) {
   return checkRating(
@@ -93,28 +71,3 @@ module.exports = {
   voyageProfitFactor,
   rating,
 };
-
-const voyage = {
-  zone: "west-indies",
-  length: 10,
-};
-const history = [
-  {
-    zone: "east-indies",
-    profit: 5,
-  },
-  {
-    zone: "west-indies",
-    profit: 15,
-  },
-  {
-    zone: "china",
-    profit: -2,
-  },
-  {
-    zone: "west-africa",
-    profit: 7,
-  },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
