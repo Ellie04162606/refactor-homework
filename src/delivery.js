fc94923a2636038a0c3dfc7586bf4d7bb49933a6
Ellie04162606 <1181140808@qq.com>
2020-08-28 23:14:27 +0800

    test : use || when delivery is undefinded

--- src/delivery.js
+++ src/delivery.js
@@ -1,23 +1,24 @@
+const rushStates = {
+  MA: (deliveryTime = 1),
+  CT: (deliveryTime = 1),
+  NY: (deliveryTime = 2),
+  NH: (deliveryTime = 2),
+};
+
+const noRushStates = {
+  MA: (deliveryTime = 2),
+  CT: (deliveryTime = 2),
+  NY: (deliveryTime = 2),
+  ME: (deliveryTime = 3),
+  NH: (deliveryTime = 3),
+};
+
 function deliveryDate(anOrder, isRush) {
   if (isRush) {
-    let deliveryTime;
-    if (["MA", "CT"].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    } else {
-      deliveryTime = 3;
-    }
+    let deliveryTime = rushStates[anOrder.deliveryState] || 3;
     return anOrder.placedOn.plusDays(1 + deliveryTime);
   } else {
-    let deliveryTime;
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    } else if (["ME", "NH"].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    } else {
-      deliveryTime = 4;
-    }
+    let deliveryTime = noRushStates[anOrder.deliveryState] || 4;
     return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
 }
