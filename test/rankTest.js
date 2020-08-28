447a826c9c53f149d70384f2052d4c7570f2e598
Ellie04162606 <1181140808@qq.com>
2020-08-28 11:55:41 +0800

    [Refactor-homework] feature: tests for voyageRisk

--- test/rankTest.js
+++ test/rankTest.js
@@ -18,4 +18,76 @@ rankTest('voyage length is 4',t=>{
   t.is(result, 1)
 });
 
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'west-indies',
+    length: 5,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 3)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'west-indies',
+    length: 9,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 4)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'china',
+    length: 4,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 5)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'china',
+    length: 5,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 7)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'china',
+    length: 9,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 8)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'east-indies',
+    length: 4,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 5)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'east-indies',
+    length: 5,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 7)
+});
+
+rankTest('voyage length is 4',t=>{
+  const voyage = {
+    zone: 'east-indies',
+    length: 9,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result, 8)
+});
+
 
